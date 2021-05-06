dea752aa73c2119a22be994ac906ed15ac669b11
Efrain May <efrain_may_75@hotmail.com>
2021-05-06 16:29:24 -0500

    drawer header + logo + svg icon dashboard

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,8 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class MainScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Row(
+          children: <Widget>[
+            Expanded(
+              //el flex esta por default en 1
+              child: Column(
+                children: <Widget>[
+                  DrawerHeader(
+                    child: Image.asset("assets/images/logo.png"),
+                  ),
+                  ListTile(
+                    leading: SvgPicture.asset("assets/icons/menu_dashbord.svg", color: Colors.white,),
+                    title: Text("Dashboard"),
+                  )
+                ],
+              ),
+            ),
+            Expanded(
+              //aqui se usa solo 5/6 dfe la pantalla completa
+              flex: 5,
+              child: Container(
+                color: Colors.blue,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
