129e3e3324b8bd9aac38edd9cd3a6d2186de46d2
Efrain May <efrain_may_75@hotmail.com>
2021-05-06 16:50:37 -0500

    drawer in the separated in the components

--- lib/screens/main/main_screen.dart
+++ lib/screens/main/main_screen.dart
@@ -1,5 +1,5 @@
+import 'package:admin/screens/main/components/side_menu.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class MainScreen extends StatelessWidget {
   @override
@@ -10,21 +10,7 @@ class MainScreen extends StatelessWidget {
           children: <Widget>[
             Expanded(
               //el flex esta por default en 1
-              child: Column(
-                children: <Widget>[
-                  DrawerHeader(
-                    child: Image.asset("assets/images/logo.png"),
-                  ),
-                  DraweListTile(title: "Dashboard",    svgSrc: "assets/icons/menu_dashbord.svg",      press: (){}),
-                  DraweListTile(title: "Transaction",  svgSrc: "assets/icons/menu_tran.svg",          press: (){}),
-                  DraweListTile(title: "Task",         svgSrc: "assets/icons/menu_task.svg",          press: (){}),
-                  DraweListTile(title: "Documents",    svgSrc: "assets/icons/menu_doc.svg",           press: (){}),
-                  DraweListTile(title: "Store",        svgSrc: "assets/icons/menu_store.svg",         press: (){}),
-                  DraweListTile(title: "Notification", svgSrc: "assets/icons/menu_notification.svg",  press: (){}),
-                  DraweListTile(title: "Profile",      svgSrc: "assets/icons/menu_profile.svg",       press: (){}),
-                  DraweListTile(title: "Settings",     svgSrc: "assets/icons/menu_setting.svg",       press: (){}),
-                ],
-              ),
+              child: SideMenu(),
             ),
             Expanded(
               //aqui se usa solo 5/6 dfe la pantalla completa
@@ -36,29 +22,6 @@ class MainScreen extends StatelessWidget {
           ],
         ),
       ),
-    );
-  }
-}
-
-class DraweListTile extends StatelessWidget {
-  
-  const DraweListTile({
-    Key key, 
-    @required this.title, 
-    @required this.svgSrc, 
-    @required this.press,
-  }) : super(key: key);
-
-  final String title, svgSrc;
-  final VoidCallback press;
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: press,
-      horizontalTitleGap: 0.0,
-      leading: SvgPicture.asset(svgSrc, color: Colors.white54, height: 16,),
-      title: Text(title, style: TextStyle(color: Colors.white54)),
     );
   }
 }
