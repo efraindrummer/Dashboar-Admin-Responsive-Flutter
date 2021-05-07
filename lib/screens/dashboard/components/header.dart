2b4a3bc565b93298dfa68cec5a0844bf74c0e01c
Efrain May <efrain_may_75@hotmail.com>
2021-05-07 00:31:24 -0500

    header

---
+++ lib/screens/dashboard/components/header.dart
@@ -0,0 +1,56 @@
+import 'package:admin/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class Header extends StatelessWidget {
+  const Header({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: <Widget>[
+        Image.asset("assets/images/profile_pic.png", height: 38),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: defaultPadding / 2),
+          child: Text("Angelina Jolic"),
+        ),
+        Icon(Icons.keyboard_arrow_down)
+      ],
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  const SearchField({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      decoration: InputDecoration(
+        hintText: "Search",
+        fillColor: secondaryColor,
+        filled: true,
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: const BorderRadius.all(Radius.circular(10)),
+        ),
+        suffixIcon: InkWell(
+          onTap: (){},
+          child: Container(
+            padding: const EdgeInsets.all(defaultPadding * 0.75),
+            margin: EdgeInsets.symmetric(horizontal: defaultPadding / 2),
+            decoration: BoxDecoration(
+              color: primaryColor,
+              borderRadius: const BorderRadius.all(Radius.circular(10)),
+            ),
+            child: SvgPicture.asset("assets/icons/Search.svg"),
+          ),
+        )
+      ),
+    );
+  }
+}
--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,4 +1,5 @@
 import 'package:admin/constants.dart';
+import 'package:admin/screens/dashboard/components/header.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -18,9 +19,7 @@ class DashboardScreen extends StatelessWidget {
               ),
               Spacer(),
               Expanded(child: SearchField()),
-              Row(
-                children: <Widget>[],
-              )
+              ProfileCard()
             ],
           )
         ],
@@ -29,34 +28,25 @@ class DashboardScreen extends StatelessWidget {
   }
 }
 
-class SearchField extends StatelessWidget {
-  const SearchField({
+class ProfileCard extends StatelessWidget {
+  const ProfileCard({
     Key key,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      decoration: InputDecoration(
-        fillColor: secondaryColor,
-        filled: true,
-        border: OutlineInputBorder(
-          borderSide: BorderSide.none,
-          borderRadius: const BorderRadius.all(Radius.circular(10)),
-        ),
-        suffixIcon: InkWell(
-          onTap: (){},
-          child: Container(
-            padding: const EdgeInsets.all(defaultPadding * 0.75),
-            margin: EdgeInsets.symmetric(horizontal: defaultPadding / 2),
-            decoration: BoxDecoration(
-              color: primaryColor,
-              borderRadius: const BorderRadius.all(Radius.circular(10)),
-            ),
-            child: SvgPicture.asset("assets/icons/Search.svg"),
-          ),
-        )
+    return Container(
+      margin: EdgeInsets.only(left: defaultPadding),
+      padding: const EdgeInsets.symmetric(
+        horizontal: defaultPadding, 
+        vertical: defaultPadding / 2
       ),
+      decoration: BoxDecoration(
+        color: secondaryColor,
+        borderRadius: const BorderRadius.all(Radius.circular(10)),
+        border: Border.all(color: Colors.white10)
+      ),
+      child: Header(),
     );
   }
 }
