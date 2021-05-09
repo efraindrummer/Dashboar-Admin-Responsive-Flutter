2064d5e9f0cb54658d5cdba51634caadf70cb63f
Efrain May <efrain_may_75@hotmail.com>
2021-05-09 00:54:29 -0500

    separed StorageInfoCard in the new component

--- lib/screens/dashboard/dashboard_screen.dart
+++ lib/screens/dashboard/dashboard_screen.dart
@@ -1,6 +1,7 @@
 import 'package:admin/constants.dart';
 import 'package:admin/screens/dashboard/components/chart.dart';
 import 'package:admin/screens/dashboard/components/header.dart';
+import 'package:admin/screens/dashboard/components/storage_info_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -55,66 +56,6 @@ class DashboardScreen extends StatelessWidget {
               )
             ],
           )
-        ],
-      ),
-    );
-  }
-}
-
-class StorageInfoCard extends StatelessWidget {
-
-  const StorageInfoCard({
-    Key key, 
-    @required this.title, 
-    @required this.svgSrc, 
-    @required this.amountOfFiles, 
-    @required this.numOfFiles,
-  }) : super(key: key);
-
-  final String title, svgSrc, amountOfFiles;
-  final int numOfFiles;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      margin: EdgeInsets.only(top: defaultPadding),
-      padding: EdgeInsets.all(defaultPadding),
-      decoration: BoxDecoration(
-        border: Border.all(
-          width: 2,
-          color: primaryColor.withOpacity(0.15),
-        ),
-        borderRadius: const BorderRadius.all(
-          Radius.circular(defaultPadding)
-        )
-      ),
-      child: Row(
-        children: [
-          SizedBox(
-            height: 20, 
-            width: 20,
-            child: SvgPicture.asset(svgSrc),
-          ),
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: defaultPadding),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    title, 
-                    maxLines: 1, 
-                    overflow: TextOverflow.ellipsis
-                  ),
-                  Text(
-                    "$numOfFiles Files",
-                    style: Theme.of(context).textTheme.caption.copyWith(color: Colors.white70)
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Text(amountOfFiles)
         ],
       ),
     );
